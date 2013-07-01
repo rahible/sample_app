68894a841eb76970be2f987285569a2fe841de80
R. Aaron Hible <rahible@gmail.com>
2013-07-01 13:11:00 -0700

    fix tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,15 @@ class User < ActiveRecord::Base
   before_save { email.downcase! }
   before_save :create_remember_token
   
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 	def create_remember_token
 		self.remember_token = SecureRandom.urlsafe_base64
 	end
+
 end
 
