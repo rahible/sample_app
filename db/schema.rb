97c80d7a9d758c8b9a742612647d215f12668ba3
R. Aaron Hible <rahible@gmail.com>
2013-07-01 11:01:59 -0700

    micropost manipulation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130606173317) do
+ActiveRecord::Schema.define(:version => 20130607144150) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
